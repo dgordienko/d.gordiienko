ac04ac248702c1d2e5e9986886d17c46a64c7da7
Dmitry Gordienko <d.gordienko@gmail.com>
2017-01-18 17:34:19 +0200

    import modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit {
   title = 'app works!';
+  ngOnInit(){
+    console.log('init application')
+  }
+   ngAfterContentInit(){
+  }
 }
